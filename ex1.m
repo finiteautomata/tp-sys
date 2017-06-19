2b05e7b0ce2591db5ecee83fe7a6e21b3d4132b6
Juan Manuel Perez <jmperez.85@gmail.com>
2017-06-19 10:30:20 -0300

    Agrego espectrogramas.

--- ex1.m
+++ ex1.m
@@ -6,40 +6,26 @@ pkg load signal;
 [x, Fs] = wavread("files/Audio.wav"); # audio file
 
 
+function do_specgram(x, Fs, window_ms, step_ms, filename='')
+    window=window_ms*Fs/1000; %20mseg
+    step=step_ms*Fs/1000; %5mseg    rebanada=ventana-overlap
+    fftn=2^nextpow2(window);
+    window
+    fftn
+    hf = figure();
+    specgram(x, fftn, Fs, window, window-step);
+    
+    if filename 
+      print(hf, filename);
+    endif
+endfunction
+
 %-----------------Banda ancha-----------------
-window=20*Fs/1000; %20mseg
-step=10*Fs/1000; %5mseg    rebanada=ventana-overlap
-fftn=2^nextpow2(window);
-figure();
-specgram(x, fftn, Fs, window, window-step);
 
 
 %----------------Banda angosta----------------
-window=67*Fs/1000; %el minimo de banda angosta
-step=5*Fs/1000; %5mseg
-fftn=2^nextpow2(window);
-figure();
-specgram(x, fftn, Fs, window, window-step);
 
-window=100*Fs/1000; %100mseg
-step; %5mseg
-fftn=2^nextpow2(window);
-figure();
-specgram(x, fftn, Fs, window, window-step);
-
-%Veo como influye el overlap:
-window; %100mseg
-step=50*Fs/1000; %5mseg
-fftn=2^nextpow2(window);
-figure();
-specgram(x, fftn, Fs, window, window-step);
-
-%Veo como influye la cantidad de fft
-window;
-step;
-fftn=2^12;
-figure();
-specgram(x, fftn, Fs, window, window-step);
-
-%%se puede observar que con una ventana de 80mseg tengo una buena resolucion en frecuencia y con un overlap de 10mseg (80-70) aumenta el ancho del tiempo
-%%%queda a definir cual es el de banda ancha (si con 20mseg estan bien)
+do_specgram(x, Fs, 16, 8, "images/specgram_w16s8.pdf");
+do_specgram(x, Fs, 34, 17, "images/specgram_w34s17.pdf");
+do_specgram(x, Fs, 68, 34, "images/specgram_w68s34.pdf");
+do_specgram(x, Fs, 100, 50, "images/specgram_w100s50.pdf");
